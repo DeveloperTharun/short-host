97e6df5f6083e25b2b712446be2a59797790f7f6
DeveloperTharun <114932079+DeveloperTharun@users.noreply.github.com>
2022-12-08 22:07:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://DeveloperTharun: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@github.com/DeveloperTharun/donff okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 donff &")
+os.system("git clone https://DeveloperTharun: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@github.com/DeveloperTharun/don12 okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 don12 &")
